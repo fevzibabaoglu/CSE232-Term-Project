dc34a506473bf16434f7a0f8340ea30afea863fa
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2023-05-08 23:17:08 +0300

    Implemented main function
    Implemented main function
    Made important variables global
    Changed parameters of parse and is_macro

--- my_mproc.c
+++ my_mproc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // global variables
 struct mac {
@@ -9,10 +10,20 @@ struct mac {
 struct mac buffer[10];  // memory buffer for 10 macro definitions
 int m_count;            // counts the number of macro definitions
 
+char field[10][7];      //max 10 fields in a line, each field max 6 characters
 
-int read(char* filename);
-void parse(char* filename);
-void is_macro();
+struct pt {
+    char mname[8];      //macro name
+    int nparams;        //number of parameters
+    char dummy[10][4];  //max. 10 parameters, each parameter max. 3 characters
+    char actual[10][4];
+};
+struct pt PT;
+
+
+int read(char *filename);
+void parse(char *file);
+void is_macro(char *argv[]);
 void expand();
 void createPT();
 
@@ -24,25 +35,40 @@ int main(int argc, char *argv[]) {
     argv[1] is the filename, argv[2], argv[3]... are condition parameters (5 and 6 in this example).
     */
 
-    if (argc == 0) return 1;
+    if (argc == 0) exit(EXIT_FAILURE);
 
     char *filename = argv[1];
-    // code...
+    char line[500];
+    FILE *file = fopen(filename, "r"); 
+    m_count = read(filename);
     
-    return 0;
+    if (!file) {
+        printf("\n Unable to open : %s ", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        parse(line);
+        is_macro(argv);
+    }
+
+    fclose(file);
+    exit(EXIT_SUCCESS);
 }
 
-int read(char* filename) {
+int read(char *filename) {
+    // update buffer[10]
+
     // code...
 }
 
-void parse(char* filename) {
-    char field[10][7]; //max 10 fields in a line, each field max 6 characters
+void parse(char *line) {
+    // update field[10][7]
 
     // code...
 }
 
-void is_macro() {
+void is_macro(char *argv[]) {
     // code...
 }
 
@@ -51,13 +77,7 @@ void expand() {
 }
 
 void createPT() {
-    struct pt {
-        char mname[8];      //macro name
-        int nparams;        //number of parameters
-        char dummy[10][4];  //max. 10 parameters, each parameter max. 3 characters
-        char actual[10][4];
-    };
-    struct pt PT;
+    // update PT
 
     // code...
 }
